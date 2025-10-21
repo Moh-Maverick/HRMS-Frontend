bef9026ba22e60a281299a35d1299f0bf830b8a4
Mohit R <mohit.radkrishnan@gmail.com>
2025-10-22 02:03:55 +0530

    firebase cred

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAcP7yY1vNEcdJyPgzClV0h_tQzTp3GCTk",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "hrms-8c8c7.firebaseapp.com",
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "hrms-8c8c7",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "hrms-8c8c7.firebasestorage.app",
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "566235119981",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:566235119981:web:47512465b8658b430e5a64",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ,
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
