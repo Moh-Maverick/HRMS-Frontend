9e518f2cc9bb8c596c1219439a7b93ab0206587b
Shreyas <shreyas4144@gmail.com>
2025-10-22 12:18:17 +0530

    build-fixes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Exclude AIinterviewBot folder from build
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/AIinterviewBot/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
